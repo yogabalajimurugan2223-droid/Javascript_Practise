008b9f9f456d75cc3f439c911bc05c54974c622b
Yoga Balaji M <yogabalajimurugan2223@gmail.com>
2026-04-28 17:12:55 +0530

    solving some problems

---
+++ dog age.js
@@ -0,0 +1,12 @@
+const myAge=56;
+let earlyYears =2;
+earlyYear= earlyYears * 10.5;
+let laterYears = myAge -2;
+laterYears*=4;
+console.log(earlyYears);
+console.log(laterYears);
+myAgeInDogYears=earlyYears+laterYears;
+myName='Bala'.toLowerCase();
+console.log(`My name is${myName}. I am ${myAge} years old in human years,which is ${myAgeInDogYears} years old in dog years.`);
+
+

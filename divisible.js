9ddea9095ba44578e8a42f3bd256bc4f39a93fbe
Yoga Balaji M <yogabalajimurugan2223@gmail.com>
2026-08-14 12:19:57 +0530

    Add files via upload

---
+++ divisible.js
@@ -0,0 +1,57 @@
+// let a = 5;
+// let b = 4;
+// if(a%b){
+//     document.write("Fizz");
+// }
+// else{
+//     doccument.write("Not");
+// }
+
+// if (a%5==0){
+//     document.write("<br>bus");
+
+// }
+// else{
+//     document>write("<br>not bus");
+// }
+
+// let a=Number(prompt("enter A value"));
+// let b=Number(prompt("Enter B value"));
+// if (a%2==0){
+//     document.write("Even");
+// }
+// else{
+//     document.write("Odd");
+// }
+
+// if (a%4==0){
+//     document.write("It is Leap Year");
+// }
+// else{
+//     document.write("It is Not Leap Year");
+// }
+
+// let a=Number(prompt("enter A value"));
+// if(a>0){
+//     document.write("it is Positive Number");
+// }
+// else if(a<0){
+//    document.write("It is Negative Number");
+// }
+// else if(a==0){
+//     document.write("Nuteral");
+// }
+
+// let b=Number(prompt("Enter B value"));
+// if(b%3==0 && b%5==0){
+//     document.write("Fizz Bus");
+// }
+// else if(b%3==0){
+//     document.write("Fizz");
+// }
+// else if(b%5==0){
+//     document.write( b );
+// }
+
+
+
